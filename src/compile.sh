dae69b272e2be3b1f6cd008fb9e40a01a8aa5df1
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-02-03 09:19:22 -0800

    added neccessary comments

--- src/compile.sh
+++ src/compile.sh
@@ -1,15 +1,18 @@
-HOME_DIR=/Users/vishaalgopalan/Desktop/Softwares/apache-tomcat-8.5.9/lib
+#HOME_DIR=path of the tomcat/lib directory
 
 export CLASSPATH=$HOME_DIR/gson-2.6.1.jar:$HOME_DIR/servlet-api.jar
 
-JDK_HOME=/usr/bin
+#JDK_HOME= jdk path
 
 $JDK_HOME/javac -d jar addToDB.java
 $JDK_HOME/javac -d jar updateWebsites.java
 
+#create a directory name "jar" with a sub-directory "syndicate" in "src" folder. this is where the class files will reside
+#after they are compiled
 cd jar
 $JDK_HOME/jar -cvf com.jar com
+#copies the jar file to the lib directory in the WEB-INF folder
 cp com.jar ../../WEB-INF/lib
+#copies the jar file to the tomcat/lib directory
 cp -rf com.jar ../../../../lib
 cd ..
-
