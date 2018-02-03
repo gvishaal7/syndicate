af67b6a7414088409b7b84a57cc0768da4806fc4
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-02-03 08:46:49 -0800

    Update addToDB.java

--- src/addToDB.java
+++ src/addToDB.java
@@ -6,11 +6,10 @@ import java.sql.*;
 import java.text.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-//import com.google.gson.*;
 
 public class addToDB extends HttpServlet {
 
-	private static final String dbDriver = "com.mysql.jdbc.Driver";
+    private static final String dbDriver = "com.mysql.jdbc.Driver";
     private static final String dbUName = ""; //DB sql username
     private static final String dbPass = ""; //DB sql password
     private static final String dbUrl = ""; //DB connection URL
@@ -25,12 +24,15 @@ public class addToDB extends HttpServlet {
     		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
 
     		Class.forName(dbDriver);
-    		con = DriverManager.getConnection(dbUrl,dbUName,dbPass);
-    		String eventName = request.getParameter("event_name").toLowerCase();
+    		con = DriverManager.getConnection(dbUrl,dbUName,dbPass); //connection object to connect with the backend
+    		String eventName = request.getParameter("event_name").toLowerCase(); //all the event names are considered to be in lowercase
     		String eventStartDate = request.getParameter("event_start_date");
     		java.util.Date startDate = sdf.parse(eventStartDate);
+		/* converts the given day to milliseconds */
     		long startDateMilli = startDate.getTime(); 
+		/* sql query to check if there is no mismatch in the event */
     		String checkSQLString = "select event_name from events where event_name='"+eventName+"' and event_end_date >="+startDateMilli;
+		/* a status is returned back to the admin depending the operation */
     		String status = "";
     		Statement queryStatement = null;
     		ResultSet sqlResponse = null;
@@ -48,7 +50,8 @@ public class addToDB extends HttpServlet {
     				String eventAddress = request.getParameter("event_address");
     				float fare = Float.parseFloat(request.getParameter("event_fare"));
     				String contactPerson = request.getParameter("contact_person");
-    				long contactNumber = Long.parseLong(request.getParameter("contact_number"));    		
+    				long contactNumber = Long.parseLong(request.getParameter("contact_number")); 
+				/* the following query ignores the id column and inserts values for the rest of the column (as id column is set to auto_increment) */
     				String sqlUpdateQuery = "INSERT INTO events (event_name, event_start_date, event_end_date, address, fare, contact_person, contact_number) VALUES ('"+eventName+"',"+startDateMilli+","+endDateMilli+",'"+eventAddress+"',"+fare+",'"+contactPerson+"',"+contactNumber+")";
     				insertStatement = con.createStatement();
     				insertStatement.executeUpdate(sqlUpdateQuery);
@@ -78,10 +81,10 @@ public class addToDB extends HttpServlet {
     				e.printStackTrace();
     			}
     		}
-    		response.setContentType("text/html");
+    		response.setContentType("text/html"); 
     		response.setCharacterEncoding("UTF-8");
     		out = response.getWriter();
-    		out.write(status);
+    		out.write(status); //writes a string back to the client
     		out.close();
     		con.close();
     	} catch(ClassNotFoundException cnfe) {
