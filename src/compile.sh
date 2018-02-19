183e8e00bf0882eab819ae2129ad1b1d76770a98
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-02-19 01:32:59 -0800

    Update compile.sh
    added the new java file

--- src/compile.sh
+++ src/compile.sh
@@ -1,11 +1,12 @@
 #HOME_DIR=path of the tomcat/lib directory
 
-export CLASSPATH=$HOME_DIR/gson-2.6.1.jar:$HOME_DIR/servlet-api.jar
+export CLASSPATH=$HOME_DIR/gson-2.6.1.jar:$HOME_DIR/servlet-api.jar:$HOME_DIR/com.jar
 
 #JDK_HOME= jdk path
 
 $JDK_HOME/javac -d jar addToDB.java
 $JDK_HOME/javac -d jar updateWebsites.java
+$JDK_HOME/javac -d jar displayEvents.java
 
 #create a directory name "jar" with a sub-directory "syndicate" in "src" folder. this is where the class files will reside
 #after they are compiled
