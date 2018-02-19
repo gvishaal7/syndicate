e9a9d5edc06ad8463c11ba1b2a0c0bc5cd5b9d58
Vishaal Gopalan <gvishaal7@users.noreply.github.com>
2018-02-19 00:27:56 -0800

    Update updateWebsites.java
    Reduced each library's size.

--- src/updateWebsites.java
+++ src/updateWebsites.java
@@ -1,23 +1,34 @@
 package com.syndicate;
 
-import java.io.*;
-import java.util.*;
-import java.sql.*;
-import java.text.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import com.google.gson.*; //google's library to convert collections to json string
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.text.SimpleDateFormat;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson; //google's library to convert collections to json string
 
 public class updateWebsites extends HttpServlet {
 
     private String dbDriver = "com.mysql.jdbc.Driver"; //mysql driver string
     private String dbName = System.getProperty("RDS_DB_NAME"); //the database name = ebdb
-    private String userName = System.getProperty("RDS_USERNAME"); // user name for mysql server = user
-    private String password = System.getProperty("RDS_PASSWORD"); // password for mysql server = password
+    private String userName = System.getProperty("RDS_USERNAME"); // user name for mysql server
+    private String password = System.getProperty("RDS_PASSWORD"); // password for mysql server
     private String hostname = System.getProperty("RDS_HOSTNAME"); // aws rds host string
     private String port = System.getProperty("RDS_PORT"); //port to which the mysql server is connected
     /* the connection string */
-    private String jdbcUrl = "jdbc:mysql://" + hostname + ":" +port + "/" + dbName + "?user=" + userNam e + "&password=" + password;
+    private String jdbcUrl = "jdbc:mysql://" + hostname + ":" +port + "/" + dbName + "?user=" + userName + "&password=" + password;
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	Connection con = null;
@@ -38,11 +49,11 @@ public class updateWebsites extends HttpServlet {
     			while(sqlResponse.next()) {
                     List<String> eventDetails = new ArrayList<>();
                     eventDetails.add(sqlResponse.getString(2));
-                    java.util.Date startDate = new java.util.Date(sqlResponse.getLong(3));
+                    Date startDate = new Date(sqlResponse.getLong(3));
                     /* converts the state date to user readable date format (MM/dd/yyyy) */
-                    String startDateStr = sdf.format(startDate); 
+                    String startDateStr = sdf.format(startDate);
                     eventDetails.add(startDateStr);
-                    java.util.Date endDate = new java.util.Date(sqlResponse.getLong(4));
+                    Date endDate = new Date(sqlResponse.getLong(4));
                     /* converts the state date to user readable date format (MM/dd/yyyy) */
                     String endDateStr = sdf.format(endDate);
                     eventDetails.add(endDateStr);
@@ -86,7 +97,7 @@ public class updateWebsites extends HttpServlet {
     		out.write(json); //writes the json string to the calling function
     		out.close();
     		con.close();
-    	} catch(ClassNotFoundException cnfe) {
+        } catch(ClassNotFoundException cnfe) {
     		cnfe.printStackTrace();
     	} catch(SQLException sqle) {
     		sqle.printStackTrace();
@@ -109,5 +120,4 @@ public class updateWebsites extends HttpServlet {
      public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
      	doPost(request,response);
      }
-
 }
